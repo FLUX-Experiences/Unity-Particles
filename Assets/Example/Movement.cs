b7846551d3d0a0650394ea3a67eee161d42e4c45
Shachar Oz <shachar.oz@gmail.com>
2019-08-03 16:14:31 +0200

    walking fire

---
+++ Assets/Example/Movement.cs
@@ -0,0 +1,67 @@
+﻿
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float _speed;
+
+    public string JumpTrigger = "";
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //controlling the player up and down, left and right
+        Move();
+
+
+        //jumping
+        if (JumpTrigger != "" && GetComponent<Animator>())
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                GetComponent<Animator>().SetTrigger(JumpTrigger);
+            }
+        }
+
+    }
+
+    public UnityEngine.UI.Text gameOverText;
+    private void OnTriggerEnter(Collider other)
+    {
+        gameOverText.gameObject.SetActive(true);
+    }
+
+
+
+
+
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * _speed);
+        }
+    }
+ }
